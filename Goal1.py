896ab088646143e4501ad023728e9c71928fe492
anastasiak36 <akelnik.9@gmail.com>
2021-02-01 14:38:35 -0500

    created window

--- Goal1.py
+++ Goal1.py
@@ -2,4 +2,17 @@ import arcade
 class MyGame(arcade.Window):
     
     def __init__(self):
+        
+        super().__init__(1000, 700, "Maze Game")
+
+        arcade.set_background_color(arcade.csscolor.LIGHT_SKY_BLUE)
+    
+    def setup(self):
         pass
+
+def main():
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+main()
