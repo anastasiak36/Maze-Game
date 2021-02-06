1691c9db4bb5a4daad8b1e61c8abd080db1078fc
anastasiak36 <akelnik.9@gmail.com>
2021-02-06 15:47:56 -0500

    updated maze manager

--- maze_manager.py
+++ maze_manager.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+import arcade
 from character_selection import Character_Selection, ImageList, Image_object
 from Goal1_Anastasia import MyGame
 
@@ -10,8 +12,24 @@ class Maze_Manager(object):
 
     def setup_character_selection(self):
         self.root.title("Select Your Character!")
-        self.image_list = ImageList("image_adress.txt")
+        self.image_list = ImageList("Maze-Game/image_adress.txt")
         self.current_screen = Character_Selection(
                                                 master = self.root, 
                                                 image_list= self.image_list, 
                                                 callback_on_selected = self.onclose_character_selection)
+    
+    def onclose_character_selection(self, selected_char_image):
+        self.current_screen.destroy()
+        self.setup_maze(selected_char_image)
+    
+    def setup_maze(self, selected_char_image):
+        window = MyGame(selected_char_image)
+        window.setup()
+        arcade.run()
+
+def main():
+    maze = Maze_Manager()
+    maze.setup_character_selection()
+    maze.root.mainloop()
+
+main()
